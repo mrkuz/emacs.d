41a162121d5278f2a4bc7f7e570d5c0bb1c1025c
mrkuz <markus@bitsandbobs.net>
2022-03-09 10:07:26 +0100

    Configure ido-like directory navigation

--- modules/vertico.el
+++ modules/vertico.el
@@ -1,3 +1,11 @@
 (use-package vertico
+  :straight '(vertico :files (:defaults "extensions/*")
+                      :includes (vertico-directory))
   :init
-  (vertico-mode))
+  (vertico-mode)
+  ;; Ido-like direcotry navigation
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
