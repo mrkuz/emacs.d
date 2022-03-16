99cafa4b6366ecffb50c970f10db217cbf9628aa
mrkuz <markus@bitsandbobs.net>
2022-03-16 20:03:58 +0100

    Add regexp-only orderless dispatcher

--- modules/orderless.el
+++ modules/orderless.el
@@ -1,6 +1,12 @@
+;; Use orderless-regexp when component ends with !
+(defun my/regexp-only (pattern _index _total)
+  (when (string-suffix-p "!" pattern)
+    `(orderless-regexp . ,(substring pattern 0 -1))))
+
 (use-package orderless
   :init
   (setq orderless-matching-styles '(orderless-literal orderless-regexp orderless-flex)
+        orderless-style-dispatchers '(my/regexp-only)
         completion-styles '(orderless)
         completion-category-defaults nil
         ;; Support for wildcards when opening files
