a2533b9adefd56e038075112676e9d934b890d32
mrkuz <markus@bitsandbobs.net>
2022-10-29 16:47:23 +0200

    Use showdown.js for Markdown preview

--- modules/my-markdown-preview.el
+++ modules/my-markdown-preview.el
@@ -7,26 +7,31 @@
 
 ;; Render GitHub-style
 (defun my//markdown-filter (buffer)
-  (princ
-   (with-temp-buffer
-     (let ((tmp (buffer-name)))
-       (set-buffer buffer)
-       (set-buffer (markdown tmp))
-       (format
-        "<!DOCTYPE html>\
-          <html>\
-            <title>Markdown preview</title>\
-            <link rel=\"stylesheet\" href = \"%s\"/>\
-            <body>\
-              <article class=\"markdown-body\"
-                       style=\"box-sizing: border-box; min-width: 200px ;max-width: 980px; margin: 0 auto; padding: 45px;\">\
-                %s\
-              </article>\
-            </body>\
-          </html>"
-        "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown-light.css"
-        (buffer-string))))
-   (current-buffer)))
+  (princ (with-current-buffer buffer
+           (format
+            "<!DOCTYPE html>\
+             <html>\
+               <head>\
+                 <title>Markdown preview</title>\
+                 <link rel=\"stylesheet\" type=\"text/css\" href=\"%s\"/>\
+                 <script src=\"%s\"></script>\
+               </head>\
+               <body>\
+                 <div id=\"content\"\
+                   class=\"markdown-body\"
+                   style=\"box-sizing: border-box; min-width: 200px ;max-width: 980px; margin: 0 auto; padding: 45px;\">%s\
+                 </div>\
+                 <script>\
+                 var converter = new showdown.Converter();\
+                 var markdown = document.getElementById('content').innerHTML;\
+                 document.getElementById('content').innerHTML = converter.makeHtml(markdown);\
+                 </script>\
+              </body>\
+            </html>"
+            "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown-light.css"
+            "https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js"
+            (buffer-substring-no-properties (point-min) (point-max))))
+         (current-buffer)))
 
 (defun my/markdown-preview ()
   "Preview markdown."
