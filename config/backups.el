dde39bd2d492201a1860ab253b34fc2f966f0e69
mrkuz <markus@bitsandbobs.net>
2022-12-03 19:16:39 +0100

    Make backups for files under VC

--- config/backups.el
+++ config/backups.el
@@ -1,5 +1,7 @@
 ;; Disable backup files
 ;; (setq make-backup-files nil)
+;; Make backups also for files under VC
+(setq vc-make-backup-files t)
 ;; Make numeric backups
 (setq version-control t)
 ;; Keep all backups
