84964ce28788b256cfc249c1feab7b90c6814d77
mrkuz <markus@bitsandbobs.net>
2022-12-09 18:33:11 +0100

    Improve org-journal-mode hour snippet

--- snippets/org-journal-mode/hour.el
+++ snippets/org-journal-mode/hour.el
@@ -2,4 +2,4 @@
 # name: hour
 # key: -@
 # --
-- [ ] ${1:$(make-string (max 0 (- 2 (string-width yas-text))) ?0)}${1:0}:00 $0
+- [ ] ${1:$(make-string (max 0 (- 2 (string-width yas-text))) ?0)}${1:0}:${2:$(make-string (max 0 (- 2 (string-width yas-text))) ?0)}${2:0} $0
