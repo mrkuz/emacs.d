dcf0289a8d64368e937124f4cc7c1b11e497bc07
mrkuz <markus@bitsandbobs.net>
2024-03-11 20:02:18 +0100

    Fix ispell configuration

--- config/flyspell.el
+++ config/flyspell.el
@@ -2,3 +2,6 @@
 (put 'ispell-dictionary 'safe-local-variable (lambda (_) t))
 ;; Inhibit default flyspell keymap
 (setq flyspell-mode-map (make-sparse-keymap))
+;; Configure hunspell
+(setq-default ispell-program-name "hunspell")
+(setenv "DICPATH" "/run/current-system/sw/share/hunspell/")
