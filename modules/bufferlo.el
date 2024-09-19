82446d616ea9dd3cba7debecdec9ef58367ca119
mrkuz <markus@bitsandbobs.net>
2024-09-19 22:02:01 +0200

    Add bufferlo (2)

--- modules/bufferlo.el
+++ modules/bufferlo.el
@@ -1,4 +1,5 @@
 (use-package bufferlo
-  :bind (([remap switch-to-buffer] . bufferlo-switch-to-buffer))
+  :bind (([remap switch-to-buffer] . bufferlo-switch-to-buffer)
+         ("C-x B" . bufferlo-find-buffer-switch))
   :config
   (bufferlo-mode 1))
