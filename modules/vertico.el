2d2e2e2ec78d3d43e45edec836b01a576bc1b95b
mrkuz <markus@bitsandbobs.net>
2026-08-17 11:22:03 +0200

    Some vertico tweaks

--- modules/vertico.el
+++ modules/vertico.el
@@ -4,5 +4,19 @@
 ;; -------------------------------------------------------------------------------------------------
 
 (use-package vertico
+  :custom
+  (vertico-count completions-max-height)
+  ;; Wrap around at either end
+  (vertico-cycle t)
   :config
   (vertico-mode 1))
+
+;; Ships with vertico, so nothing to install
+(use-package vertico-directory
+  :ensure nil
+  :after vertico
+  ;; Shorten shadowed paths, as when a second `~' or `/' is typed
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+  :bind (:map vertico-map
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word)))
